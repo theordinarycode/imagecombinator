478a34fa794a882c08d56267d676472e86d222db
theordinarycode <ordinarycodeemail@gmail.com>
2022-03-08 09:08:00 +0700

    progress on images

--- src/App.js
+++ src/App.js
@@ -1,58 +1,48 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
+import cat from "./images/cat.jpeg";
+import frame from "./images/frame.png";
 
 function App() {
   const [image, setImage] = useState(null);
   const [image2, setImage2] = useState(null);
   const [image3, setImage3] = useState(null);
-  const canvas = useRef(null);
+  // const [canvas, setCanvas] = useState();
   const canvas2 = useRef(null);
   const canvas3 = useRef(null);
 
   useEffect(() => {
-    const catImage = new Image();
-    catImage.src = "https://d2ph5fj80uercy.cloudfront.net/04/cat3187.jpg";
-    console.log(catImage.src);
-    catImage.onload = () => setImage(catImage);
-    //
-    const xLayer = new Image();
-    xLayer.src = "https://thiscatdoesnotexist.com";
-    xLayer.onload = () => setImage2(xLayer);
-    //
-    const yLayer = new Image();
-    yLayer.src = "https://thiscatdoesnotexist.com";
-    yLayer.onload = () => setImage3(yLayer);
+    draw();
   }, []);
 
-  useEffect(() => {
-    if (image && canvas) {
-      const ctx = canvas.current.getContext("2d");
-      ctx.fillStyle = "black";
-      ctx.fillRect(0, 0, 800, 856 + 80);
-      ctx.drawImage(image, 80 / 2, 20);
-      //
-      const ctx2 = canvas2.current.getContext("2d");
-      ctx2.fillStyle = "black";
-      ctx2.fillRect(0, 0, 100, 100);
-      ctx2.drawImage(image2, 0, 0, 100, 100);
-      //
-      const ctx3 = canvas3.current.getContext("2d");
-      ctx2.fillStyle = "black";
-      ctx2.fillRect(0, 0, 100, 100);
-      ctx2.drawImage(image3, 0, 0, 100, 100);
-    }
-  }, [image, canvas]);
+  function draw() {
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+    //Draw slice
+    ctx.drawImage(
+      document.getElementById("source"),
+      33,
+      71,
+      104,
+      124,
+      21,
+      20,
+      87,
+      104
+    );
+
+    //Draw frame
+    ctx.drawImage(document.getElementById("frame"), 0, 0);
+  }
 
   return (
     <div className="App">
-      <h1>Cat Meme</h1>
-      <div>
-        <canvas ref={canvas} width={600} height={456 + 80} />
+      <canvas id="canvas" width="150" height="150"></canvas>
+      <div style={{ display: "none" }}>
+        <img id="frame" src={frame} width="132" height="150" />
+        <img id="source" src={cat} width="300" height="227" />
       </div>
-      {/*  */}
-      <canvas ref={canvas2} id="layer1" width="100" height="100"></canvas>
-      <canvas ref={canvas3} id="layer2" width="100" height="100"></canvas>
-      {/* <img src='.images/cat3187.jpeg' /> */}
     </div>
   );
 }
